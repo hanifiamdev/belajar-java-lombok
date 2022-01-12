aa4f14df7ee9e1b604e3ff30a780bffd4dabdcc6
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-13 01:41:52 +0700

    Cleanup

---
+++ src/test/java/com/hanifiamdev/lombok/FileHelperTest.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.lombok;
+
+import org.junit.jupiter.api.Test;
+
+public class FileHelperTest {
+
+    @Test
+    void testPom() throws Exception {
+        String text = FileHelper.loadFile("pom.xml");
+        System.out.println(text);
+    }
+
+}
