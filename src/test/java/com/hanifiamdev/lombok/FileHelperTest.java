c9fa382cfbb10feec83ea3d8c84a05758f56e2aa
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-13 01:57:28 +0700

    Sneaky Throws

--- src/test/java/com/hanifiamdev/lombok/FileHelperTest.java
+++ src/test/java/com/hanifiamdev/lombok/FileHelperTest.java
@@ -1,6 +1,9 @@
 package com.hanifiamdev.lombok;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
 
 public class FileHelperTest {
 
@@ -8,6 +11,13 @@ public class FileHelperTest {
     void testPom() throws Exception {
         String text = FileHelper.loadFile("pom.xml");
         System.out.println(text);
+    }
+
+    @Test
+    void testFileNotFound() {
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+            FileHelper.loadFile("tidakada.txt");
+        });
     }
 
 }
